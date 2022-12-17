63f386e7cf1d70fe51c9407b295a11d0666e18c9
riad.alsaqal <r.saqal@workwithnerds.ca>
2022-12-17 15:34:00 +0300

    init the projcet

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from "react-native-paper";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+import Index from "./index";
+
+const client = new ApolloClient({
+  uri: "https://abdelwahapbak.pythonanywhere.com/graphql",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ApolloProvider client={client}>
+      <PaperProvider>
+        <Index />
+      </PaperProvider>
+    </ApolloProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
